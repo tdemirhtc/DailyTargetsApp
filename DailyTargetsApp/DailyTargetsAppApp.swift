dcfe3a2b74fb38e2bea51be2d6ae495b4cf7b830
Hatice Taşdemir <tdemirhtc@gmail.com>
2024-11-15 11:28:07 +0300

    Initial Commit

---
+++ DailyTargetsApp/DailyTargetsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyTargetsAppApp.swift
+//  DailyTargetsApp
+//
+//  Created by Hatice Taşdemir on 15.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct DailyTargetsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
